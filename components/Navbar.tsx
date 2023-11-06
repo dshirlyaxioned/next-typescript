2dfe0c4142e920dc5628225a7422405e54f5a29e
dshirlyaxioned <shirlyd@axioned.com>
2023-11-06 16:51:00 +0530

    feat: footer section done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { CustomButton } from '.'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { CustomButton } from ".";
 
 const Navbar = () => {
   return (
-    <header className='w-full absolute z-10'>
-      <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'>
-        <Link href="/" className='flex justify-center items-center'>
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <Link href="/" className="flex justify-center items-center">
           <Image
             src="/logo.svg"
             alt="Car Hub Logo"
             width={118}
             height={18}
-            className='object-contain'
+            className="object-contain"
           />
         </Link>
-        <CustomButton title="Sign In" btnType="button" containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]' />
-
+        <CustomButton
+          title="Sign In"
+          btnType="button"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px] lg:hover:text-[#172b7c] transition"
+        />
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
