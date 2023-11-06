f7ed0af2c5891cf6a6511e295bd016176009525b
dshirlyaxioned <shirlyd@axioned.com>
2023-11-06 19:48:20 +0530

    feat: car catalouge wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,44 @@
-import { Hero } from '../components';
-import Image from 'next/image'
+import { CarCard, CustomFilter, Hero, SearchBar } from "../components";
+import Image from "next/image";
+import { fetchCars } from "../utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  console.log(allCars);
+
+  const isDataEmty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xal font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+
+        {!isDataEmty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car, i) => (
+                <CarCard car={car} key={i} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">oops, no result</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
     </main>
-  )
+  );
 }
